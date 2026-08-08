6afba2bc4951675142ebe7264cf7410be6951b8e
gabo <cc22060@ues.edu.sv>
2026-08-08 15:38:20 -0600

    feat: implementar clase Prestamo

--- classes/Prestamo.php
+++ classes/Prestamo.php
@@ -9,39 +9,44 @@ class Prestamo {
     private $estado;
 
     public function __construct($libro_id = null, $usuario_id = null) {
-        // TODO: Inicializar atributos, establecer fecha_prestamo a hoy
+        // Inicializar atributos, establecer fecha_prestamo a hoy
+        $this->libro_id = $libro_id;
+        $this->usuario_id = $usuario_id;
+        $this->fecha_prestamo = date('Y-m-d');
+        $this->fecha_devolucion = null;
+        $this->estado = 'activo';
     }
 
     // Getters y Setters
     public function getId() {
-        // TODO
+        return  $this->id;
     }
 
     public function getLibroId() {
-        // TODO
+        return  $this->libro_id;
     }
 
     public function getUsuarioId() {
-        // TODO
+        return $this->usuario_id;
     }
 
     public function getFechaPrestamo() {
-        // TODO
+        return  $this->fecha_prestamo;
     }
 
     public function getFechaDevolucion() {
-        // TODO
+        return $this->fecha_devolucion;
     }
 
     public function setFechaDevolucion($fecha) {
-        // TODO
+        $this->fecha_devolucion = $fecha;
     }
 
     public function getEstado() {
-        // TODO
+        return $this->estado;
     }
 
     public function setEstado($estado) {
-        // TODO
+        $this->estado = $estado;
     }
 }
